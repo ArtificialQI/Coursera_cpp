b77d2d8328373c9d26102c8f23f66ef3c7a89613
ArtificialQI <68777381+ArtificialQI@users.noreply.github.com>
2021-01-31 21:25:12 +0300

    commit

--- oop/Final/main.cpp
+++ oop/Final/main.cpp
@@ -1,43 +1,32 @@
-#include <iostream>
-#include <sstream>
-#include <map>
-#include <set>
-using namespace std;
-
-// Реализуйте функции и методы классов и при необходимости добавьте свои
+// Р РµР°Р»РёР·СѓР№С‚Рµ С„СѓРЅРєС†РёРё Рё РјРµС‚РѕРґС‹ РєР»Р°СЃСЃРѕРІ Рё РїСЂРё РЅРµРѕР±С…РѕРґРёРјРѕСЃС‚Рё РґРѕР±Р°РІСЊС‚Рµ СЃРІРѕРё
 
 class Date {
 public:
-    int GetYear() const { return year; };
-    int GetMonth() const { return month; };
-    int GetDay() const { return day; };
-private:
-    int year;
-    int month;
-    int day;
+  int GetYear() const;
+  int GetMonth() const;
+  int GetDay() const;
 };
 
 bool operator<(const Date& lhs, const Date& rhs);
 
 class Database {
 public:
-    void AddEvent(const Date& date, const string& event);
-    bool DeleteEvent(const Date& date, const string& event);
-    int  DeleteDate(const Date& date);
+  void AddEvent(const Date& date, const string& event);
+  bool DeleteEvent(const Date& date, const string& event);
+  int  DeleteDate(const Date& date);
 
-    /* ??? */ //Find(const Date& date) const;
-
-    void Print() const;
-public:
-    map<Date, set<string>> Data;
+  /* ??? */ Find(const Date& date) const;
+  
+  void Print() const;
 };
 
 int main() {
-    Database db;
-    string command;
-    while (getline(cin, command)) {
-        // Считайте команды с потока ввода и обработайте каждую
-    }
+  Database db;
+    
+  string command;
+  while (getline(cin, command)) {
+    // РЎС‡РёС‚Р°Р№С‚Рµ РєРѕРјР°РЅРґС‹ СЃ РїРѕС‚РѕРєР° РІРІРѕРґР° Рё РѕР±СЂР°Р±РѕС‚Р°Р№С‚Рµ РєР°Р¶РґСѓСЋ
+  }
 
-    return 0;
+  return 0;
 }
