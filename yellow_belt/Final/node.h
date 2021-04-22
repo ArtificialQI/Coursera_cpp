c98799a574fd18a5cc4415a7f55938545da5b721
ArtificialQI <68777381+ArtificialQI@users.noreply.github.com>
2021-04-22 05:39:04 +0300

    commit

--- yellow_belt/Final/node.h
+++ yellow_belt/Final/node.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#include "date.h"
+
+enum class Comparison {
+	Less,
+	LessOrEqual,
+	Greater,
+	GreaterOrEqual,
+	Equal,
+	NotEqual,
+
+};
+
 class Node {
 public:
 	virtual int Evaluate() const = 0;
@@ -12,12 +24,20 @@ public:
 
 class DateComparisonNode : public Node {
 public:
+	DateComparisonNode(Comparison& cmp, Date& date) : cmp_(cmp), date_(date) {}
 	int Evaluate() const override;
+private:
+	const Comparison cmp_;
+	const Date date_;
 };
 
 class EventComparisonNode : public Node {
 public:
+	EventComparisonNode(const Comparison& cmp, const string& value) : cmp_(cmp), value_(value) {}
 	int Evaluate() const override;
+private:
+	const Comparison cmp_;
+	const string value_;
 };
 
 class LogicalOperationNode : public Node {
