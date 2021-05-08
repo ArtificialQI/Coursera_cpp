9a79559604abe154eeb5ecdb445cde38b1f94dfc
ArtificialQI <68777381+ArtificialQI@users.noreply.github.com>
2021-05-08 15:39:59 +0300

    commit

--- yellow_belt/Final/node.h
+++ yellow_belt/Final/node.h
@@ -3,6 +3,7 @@
 #include "date.h"
 
 #include <string>
+#include <map>
 #include <set>
 #include <utility>
 #include <memory>
@@ -24,28 +25,36 @@ enum class LogicalOperation {
 };
 class Node {
 public:
-	virtual bool Evaluate(const Date& date, const string& event) const = 0;
+	virtual vector<pair<Date, set<string>>> Evaluate(const Date& date, const string& event) const = 0;
 };
  
 class EmptyNode : public Node {
 public:
-	bool Evaluate(const Date& date, const string& event) const override;
+	vector<pair<Date, set<string>>> Evaluate(const Date& date, const string& event) const override;
 };
 
 class DateComparisonNode : public Node {
 public:
 	DateComparisonNode(Comparison& cmp, Date& date) : cmp_(cmp), date_(date) {}
-	template<class RandomIt>
-	pair<RandomIt, RandomIt> Evaluate(const Date& date_, const string& event) const override {
-		pair<RandomIt, RandomIt> predicate = [date_, event](map<Date, set<string>> storage) {
+
+		template <class RandomIt>
+		vector<pair<Date, vector<RandomIt>>> Evaluate(const Date& date_, const string& event) const override {
+		vector<pair<Date, vector<RandomIt>>> predicate = [&](map<Date, set<string>> storage) {
 			auto begin = storage.begin();
-			auto end = storage.end();
+			auto end = storage.end(); 
 			auto lower = storage.lower_bound(date_);
 			auto upper = storage.upper_bound(date_);
-			auto find_ = find(date_);
+			auto find_ = storage.find(date_);
+			vector<pair<Date, vector<RandomIt>>> data;
 			switch (cmp_) {
 			case Comparison::Less:
-				return {begin, --lower};
+				for (auto it = begin; it != lower; ++it) {
+					pair<Date, vector<RandomIt>> item;
+					item.first = it->first;
+					item.second = {it->second.begin(), it->second.end()};
+					data.emplace(item);
+				}
+				return data; 
 				break;
 			case Comparison::LessOrEqual:
 				return {begin, lower};
@@ -61,7 +70,7 @@ public:
 				break;
 			case Comparison::NotEqual:
 				if (find_ = end)
-					return {begin, --end}
+					return {begin, --end};
 				storage.erase(find_);
 				return {begin, --end};
 				break;
@@ -69,7 +78,7 @@ public:
 				break;
 			}
 		};
-		return predicate;
+		return predicate; 
 	} 
 private:
 	const Comparison cmp_;
@@ -79,9 +88,11 @@ private:
 class EventComparisonNode : public Node {
 public:
 	EventComparisonNode(const Comparison& cmp, const string& event) : cmp_(cmp), event_(event) {}
+	//template<class RandomIt>
+	//
 	template<class RandomIt>
-	pair<RandomIt, RandomIt> Evaluate(const Date& date, const string& event_) const override {
-		pair<RandomIt, RandomIt> predicate = [date, event_](map<Date, set<string>> storage) {
+	vector<pair<RandomIt, vector<RandomIt>>> Evaluate(const Date& date, const string& event_) const override {
+		vector<pair<RandomIt, vector<RandomIt>>> predicate = [date, event_](map<Date, set<string>> storage) {
 			auto begin = storage.begin();
 			auto end = storage.end();
 
@@ -119,9 +130,18 @@ public:
 	Function Evaluate(const Date& date, const string& event) const override {
 
 		switch (log_)
-		{
+		{ 
 		case LogicalOperation::And:
-			/* code */
+			auto lhs = left->Evaluate();
+			auto rhs = right->Evaluate();
+			vector<pair<Date, vector<RandomIt>>> result;
+
+			for (int i = 0; i != lhs.size() || i != rhs.size(); ++i) {
+				if (lhs[i].first == rhs[i].first) {
+					for ()
+				}
+			}
+			return result;
 			break;
 		case LogicalOperation::Or:
 
@@ -132,7 +152,7 @@ public:
 		}
 	}
 private:
-	LogicalOperation log_;
-	shared_ptr<Node> left_;
-	shared_ptr<Node> right_;
+	const LogicalOperation log_;
+	const shared_ptr<Node> left_;
+	const shared_ptr<Node> right_;
 };
