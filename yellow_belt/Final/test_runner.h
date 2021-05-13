eb682255fbb549073c45ae97ad0dd7b6ecffc443
ArtificialQI <68777381+ArtificialQI@users.noreply.github.com>
2021-05-13 11:58:49 +0300

    test_runner_commit

--- yellow_belt/Final/test_runner.h
+++ yellow_belt/Final/test_runner.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <sstream>
-#include <exception>
+#include <stdexcept>
 #include <iostream>
 #include <string>
 #include <map>
@@ -9,7 +9,29 @@
 using namespace std;
 
 template <class T>
-ostream& operator << (ostream& os, const set<T>& s) {
+ostream& operator<< (ostream& os, const set<T>& s);
+
+template <class K, class V>
+ostream& operator<< (ostream& os, const map<K, V>& m);
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint);
+
+void Assert(bool b, const string& hint);
+
+class TestRunner {
+public:
+    template <class TestFunc>
+    void RunTest(TestFunc func, const string& test_name);
+    TestRunner();
+    ~TestRunner();
+
+private:
+    int fail_count;
+};
+
+template <class T>
+ostream& operator<< (ostream& os, const set<T>& s) {
     os << "{";
     bool first = true;
     for (const auto& x : s) {
@@ -23,7 +45,7 @@ ostream& operator << (ostream& os, const set<T>& s) {
 }
 
 template <class K, class V>
-ostream& operator << (ostream& os, const map<K, V>& m) {
+ostream& operator<< (ostream& os, const map<K, V>& m) {
     os << "{";
     bool first = true;
     for (const auto& kv : m) {
@@ -37,9 +59,7 @@ ostream& operator << (ostream& os, const map<K, V>& m) {
 }
 
 template<class T, class U>
-void AssertEqual(const T& t, const U& u,
-    const string& hint)
-{
+void AssertEqual(const T& t, const U& u, const string& hint) {
     if (t != u) {
         ostringstream os;
         os << "Assertion failed: " << t << " != " << u
@@ -48,31 +68,14 @@ void AssertEqual(const T& t, const U& u,
     }
 }
 
-inline void Assert(bool b, const string& hint) {
-    AssertEqual(b, true, hint);
+template <class TestFunc>
+void TestRunner::RunTest(TestFunc func, const string& test_name) {
+    try {
+        func();
+        cerr << test_name << " OK" << endl;
+    }
+    catch (runtime_error& e) {
+        ++fail_count;
+        cerr << test_name << " fail: " << e.what() << endl;
+    }
 }
-
-class TestRunner {
-public:
-    template <class TestFunc>
-    void RunTest(TestFunc func, const string& test_name) {
-        try {
-            func();
-            cerr << test_name << " OK" << endl;
-        }
-        catch (runtime_error& e) {
-            ++fail_count;
-            cerr << test_name << " fail: " << e.what() << endl;
-        }
-    }
-
-    ~TestRunner() {
-        if (fail_count > 0) {
-            cerr << fail_count << " unit tests failed. Terminate" << endl;
-            exit(1);
-        }
-    }
-
-private:
-    int fail_count = 0;
-};
