0c57aa0a8f7f3f932d059e70de2285655e607ce2
ArtificialQI <68777381+ArtificialQI@users.noreply.github.com>
2021-06-19 13:42:13 +0300

    commit memory

---
+++ red_belt/memory/objects_pool/object_pool.cpp
@@ -0,0 +1,86 @@
+#include "test_runner.h"
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stdexcept>
+#include <set>
+using namespace std;
+
+template <class T>
+class ObjectPool {
+public:
+  T* Allocate() {
+    if (!deallocated.empty()) {
+      auto ptr = deallocated.front();
+      allocated.insert(ptr);
+      deallocated.pop();
+      return ptr;
+    } else {
+      T* ptr = new T;
+      allocated.insert(ptr);
+      return ptr;
+    }
+  }
+  T* TryAllocate() {
+    if (!deallocated.empty()) {
+      auto ptr = deallocated.front();
+      allocated.insert(ptr);
+      deallocated.pop();
+      return ptr;
+    } else {
+      return nullptr;
+    }
+  }
+
+  void Deallocate(T* object) {
+    if (allocated.count(object) != 0) {
+      allocated.erase(object);
+      deallocated.push(object);      
+    } else {
+      throw invalid_argument("invalid_argument");
+    }
+  }
+
+  ~ObjectPool() {
+    for(auto it = allocated.begin(); it != allocated.end(); ++it) {
+      delete *it;
+    }
+    while (!deallocated.empty()) {
+      delete deallocated.front();
+      deallocated.pop();
+    }
+  }
+
+private:
+  set<T*> allocated;
+  queue<T*> deallocated;
+};
+
+void TestObjectPool() {
+  ObjectPool<string> pool;
+
+  auto p1 = pool.Allocate();
+  auto p2 = pool.Allocate();
+  auto p3 = pool.Allocate();
+
+  *p1 = "first";
+  *p2 = "second";
+  *p3 = "third";
+
+  pool.Deallocate(p2);
+  ASSERT_EQUAL(*pool.Allocate(), "second");
+  pool.Deallocate(p3);
+  pool.Deallocate(p1);
+  ASSERT_EQUAL(*pool.Allocate(), "third");
+  ASSERT_EQUAL(*pool.Allocate(), "first");
+
+  pool.Deallocate(p1);
+}
+
+int main() {
+  TestRunner tr;
+  RUN_TEST(tr, TestObjectPool);
+  return 0;
+}
